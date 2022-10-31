e83876dc20d4c97deabd6ff8027c1ba52d72c851
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-10-31 15:32:01 +0100

    added new tests

--- greet.test.js
+++ greet.test.js
@@ -5,4 +5,13 @@ describe("greet", () => {
     const result = greet("Stefan");
     expect(result).toBe("Hello Stefan!");
   });
+  test('returns a string "Hello Currywurst" if called greet()', () => {
+    const result = greet();
+    expect(result).toBe("Hello Currywurst!");
+  });
+  test('returns a string "Hello Coach!" if called greet("Thomas") or greet("Stefan")', () => {
+    const result = greet("Thomas");
+    expect(result).toBe("Hello Coach");
+    expect(greet("Stefan")).toBe("Hello Coach!");
+  });
 });
